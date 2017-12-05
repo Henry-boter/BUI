d84748a82d760d18ccdb0cd23f1f6b23f9a7f1a7
BOB <1971785019@qq.com>
2017-12-05 09:42:18 +0800

    vue 全家桶

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -10,11 +10,11 @@
       <br>
       <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
-    <h2>Ecosystem</h2>
+    <h2>Vue 全家桶</h2>
     <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://router.vuejs.org/zh-cn/" target="_blank">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org/zh-cn/" target="_blank">vuex</a></li>
+      <li><a href="https://vue-loader.vuejs.org/zh-cn/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
   </div>
