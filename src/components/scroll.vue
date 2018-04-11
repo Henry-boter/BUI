1d8408606e4e4b9d03cea2e668c5f12949eae047
Henry-boter <1971785019@qq.com>
2018-04-11 22:46:16 +0800

    j

---
+++ src/components/scroll.vue
@@ -0,0 +1,38 @@
+<template>
+  <div ref="wrapper">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+  export default {
+    name: 'wrapper',
+    props: {
+      probeType: {
+        type: Number,
+        default: 1
+      },
+      click: {
+        type: Boolean,
+        default: true
+      },
+      listenScroll: {
+        type: Boolean,
+        default: false
+      },
+      data: {
+        type: Array,
+        default: null
+      }
+    },
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+<style scoped>
+
+</style>
